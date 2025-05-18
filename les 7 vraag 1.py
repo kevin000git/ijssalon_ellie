9780cb78dc61973a7a34a1a405ae0b73e241bddd
kevin000git <kevin.v.vliet@gmail.com>
2025-05-18 18:47:07 +0200

    Add files via upload

---
+++ les 7 vraag 1.py
@@ -0,0 +1,14 @@
+smaken = ["aardbei", "vanille", "chocolade"]
+#week_aanbieding = smaken[0]+"-ijs,"
+#prijs = 3.00
+#aanbieding_1 = (prijs * 0.8)
+#print (aanbieding_1)
+#aanbieding_1 = "%.2f" % aanbieding_1
+#vraag 1 t/m 1.5    
+#tekst_bord = "Vandaag in de aanbieding:", week_aanbieding, "1 liter - slechts €", aanbieding_1
+#vraag 1.6
+#print (f"vandaag in de aanbieding: {week_aanbieding} 1 liter - slechts € {aanbieding_1}".swapcase())
+#vraag 1.7
+print (f"wij verkopen de volgende smaken:, {smaken}")
+for item in smaken:
+    print(f"wij verkopen de volgende smaken: {item}")
